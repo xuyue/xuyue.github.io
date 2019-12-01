7534b541060266ef960598cf4d087ecb2a2ea2cc
xuyue <xuyue@rawstonedu.com>
2019-12-01 20:12:20 +0800

    Site updated: 2019-12-01 20:12:20

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@
 
     if (typeof (moment) === 'function') {
         $('.article-meta time').each(function () {
-            $(this).text(moment($(this).attr('datetime')).fromNow());
+            $(this).text(moment($(this).attr('datetime')).format('YYYY-MM-DD'));
         });
     }
 
